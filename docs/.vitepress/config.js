3793646260385179d512e2f8a7a8345e15e2d0ef
fraxer <fraxer@mail.ru>
2023-07-09 21:55:03 +0700

    feat: add yandex metrika

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,6 +4,31 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Cpdy",
   description: "Cpdy docs",
+  head: [
+    [
+      'script',
+      { id: 'mtrk' },
+      `
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(94252971, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+        });
+      `
+    ],
+    [
+      'div',
+      {},
+      `<img src="https://mc.yandex.ru/watch/94252971" style="position:absolute; left:-9999px;" alt="" />`
+    ]
+  ],
   locales: {
     root: {
       label: 'Русский',
